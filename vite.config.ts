073572b73afe39c731fd6573b06b0b42a0e2873b
Felipe Aguilera Bertolini <61721430+feaguilera@users.noreply.github.com>
2026-02-17 21:14:28 -0300

    att

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,13 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   define: {
-    'process.env': {}
+    'process.env': {
+      API_KEY: JSON.stringify(process.env.API_KEY || '')
+    }
   },
   build: {
     outDir: 'dist',
-    sourcemap: false
+    sourcemap: false,
+    emptyOutDir: true
   }
 });
